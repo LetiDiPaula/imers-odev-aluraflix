0a1d374facfcedfb8cd0f2a9225a3b0b42662860
LetiDiPaula <96254586+LetiDiPaula@users.noreply.github.com>
2021-12-16 14:13:09 -0300

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,9 @@
+var listaFilmes = [
+  "https://br.web.img3.acsta.net/pictures/210/058/21005876_20130514222922413.jpg",
+  "https://br.web.img3.acsta.net/pictures/15/05/14/14/20/365361.jpg",
+  "https://m.media-amazon.com/images/I/71WOojK8IcL._AC_SL1476_.jpg"
+];
+
+for (var i = 0; i < listaFilmes.length; i++) {
+  document.write("<img src=" + listaFilmes[i] + ">");
+}
